dea2dd80032fc78e59fa058af429ad3c61ba8ebc
Chexxxor <chexxor@outlook.com>
2016-04-10 04:53:02 +0200

    Task 2, Version 0.2

--- Oblig4_V2016/src/task2/TowerOfHanoi.java
+++ Oblig4_V2016/src/task2/TowerOfHanoi.java
@@ -3,31 +3,35 @@ package task2;
 import java.util.Scanner; 
 
 public class TowerOfHanoi {
-  /** Main method */
-  public static void main(String[] args) {
-    // Create a Scanner
-    Scanner input = new Scanner(System.in);
-    System.out.print("Antall skiver: ");
-    int n = input.nextInt();
-    input.close();
+	private static int moves; 
+	/** Main method */
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		System.out.print("Antall skiver: ");
+		int n = input.nextInt();
+		input.close();
 
-    // Find the solution recursively
-    System.out.println("Flyttingene blir da:");
-    moveDisks(n, 'A', 'B', 'C');
-  }
+		// Find the solution recursively
+		System.out.println("Flyttingene blir da:");
+		moves = 0;
+		moveDisks(n, 'A', 'B', 'C');
+		System.out.println("Antall flyttinger: " + moves);
+	}
 
-  /** The method for finding the solution to move n disks
+	/** The method for finding the solution to move n disks
       from fromTower to toTower with auxTower */
-  public static void moveDisks(int n, char fromTower,
-      char toTower, char auxTower) {
-    if (n == 1) // Stopping condition
-      System.out.println("Flytter skive " + n + " fra " +
-        fromTower + " til " + toTower);
-    else {
-      moveDisks(n - 1, fromTower, auxTower, toTower);
-      System.out.println("Flytter skive " + n + " fra " +
-        fromTower + " til " + toTower);
-      moveDisks(n - 1, auxTower, toTower, fromTower);
-    }
-  }
+	public static void moveDisks(int n, char fromTower,
+			char toTower, char auxTower) {
+		moves++;
+		if (n == 1) // Stopping condition
+			System.out.println("Flytter skive " + n + " fra " +
+					fromTower + " til " + toTower);
+		else {
+			moveDisks(n - 1, fromTower, auxTower, toTower);
+			System.out.println("Flytter skive " + n + " fra " +
+					fromTower + " til " + toTower);
+			moveDisks(n - 1, auxTower, toTower, fromTower);
+		}
+	}
 }
