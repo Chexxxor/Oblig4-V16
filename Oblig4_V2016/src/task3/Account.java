dc23e5282f165d36fa55dfbc050fddaac4745fcd
Kenneth Johan Kristensen <chexxor@outlook.com>
2016-04-24 06:28:16 +0200

    Task 3, bank and accounts

--- Oblig4_V2016/src/task3/Account.java
+++ Oblig4_V2016/src/task3/Account.java
@@ -4,43 +4,43 @@ public class Account extends Thread {
 	private Bank b;
 	private int id;
 	private int amount;
+	boolean running;
 
 	public Account(Bank b, int amount, int id){
 		this.id = id;
 		this.b = b;
 		this.amount = amount;
-		this.start();
+		running = true;
 	}
 	
 	public int accountId(){
 		return id;
 	}
-	
 	public int saldo(){
 		return amount;
 	}
 	
-	public synchronized void deposit(int amount){
-		amount++;
+	public void deposit(int amount){
+		this.amount += amount;
 	}
-
-	public synchronized void withdraw(int amount){
-		amount--;
+	public void withdraw(int amount){
+		this.amount -= amount;
 	}
 	
+	public void kill(){
+		running = false;
+	}
 	@Override
 	public void run(){
-		while(true){
-			synchronized (this) {
-				Account toAccount = this;
-				while(toAccount == this){
-					toAccount = b.getRandom();
-				}
-				b.transfer(this, toAccount, (int) (Math.random() * Bank.INITIAL_BALANCE));
-				try {
-					wait((long) (Math.random() * 5000));
-				} catch (InterruptedException e) {}
+		while(running){
+			Account toAccount = this;
+			while(toAccount == this){
+				toAccount = b.getRandom();
 			}
+			try {
+				sleep((long) (Math.random() * Bank.BREAK_TIME));
+				b.transfer(this, toAccount, (int) (Math.random() * Bank.INITIAL_BALANCE));
+			} catch (InterruptedException e) {}
 		}
 	}
 
--- Oblig4_V2016/src/task3/Bank.java
+++ Oblig4_V2016/src/task3/Bank.java
@@ -3,13 +3,18 @@ package task3;
 public class Bank {
 	public static final int NACCOUNTS = 10;
 	public static final int INITIAL_BALANCE = 10000;
+	public static final int NTEST = 50;
+	public static final int BREAK_TIME = 50000;
 	private Account[] accounts = new Account[NACCOUNTS];
+	private int nTransactions;
 	
-	public static void main(String[] args){
-		new Bank();
+	public static void main(String[] args) throws InterruptedException{
+		Bank b = new Bank();
+		b.run();
 	}
 	
 	public Bank(){
+		nTransactions = 0;
 		for(int i = 0; i < NACCOUNTS; i++){
 			accounts[i] = new Account(this, INITIAL_BALANCE, i);
 			accounts[i].setPriority(Thread.NORM_PRIORITY + i % 2);
@@ -19,7 +24,6 @@ public class Bank {
 	public Account getAccount(int i) throws ArrayIndexOutOfBoundsException {
 		return accounts[i];
 	}
-	
 	public Account getRandom(){
 		return accounts[(int) (Math.random() * NACCOUNTS)];
 	}
@@ -28,21 +32,42 @@ public class Bank {
 		return NACCOUNTS;
 	}
 	
-	public void transfer(Account fromAccount, Account toAccount, int amount){
-		System.out.println("Initializing transfer of " + amount +
+	public void transfer(Account fromAccount, Account toAccount, int amount) throws InterruptedException{
+		System.out.println(nTransactions + ": Initializing transfer of £" + amount +
 				" from #" + fromAccount.accountId() +
 				" to #" + toAccount.accountId());
-		while(fromAccount.saldo() < amount){
-			System.out.println("Insufficent funds on #" + fromAccount.accountId());
-			try {
-				wait(1000);
-			} catch (InterruptedException e) {}
+		synchronized (fromAccount) { //Withdraw
+			while(fromAccount.saldo() < amount){
+				System.out.println("Insufficent funds on #" + fromAccount.accountId() + ", need £" + (amount - fromAccount.saldo()) + " more");
+				fromAccount.wait();
+				System.out.println("Transfer recieved on #" + fromAccount.accountId());
+				if(fromAccount.saldo() >= amount)
+					System.out.println("Continuing transaction from #" + fromAccount.accountId() + " to #" + toAccount.accountId());
+				else
+					System.out.print("Still ");
+			}
+			fromAccount.withdraw(amount);
 		}
-		fromAccount.withdraw(amount);
-		System.out.println("Credit on hold");
-		toAccount.deposit(amount);
-		System.out.println("Transfer complete");
-		//toAccount.notify();
+		//Money will temporarily disappear from one account before appearing on another
+		//this will sometimes seem like "missing money" during the tests  
+		//System.out.println(nTransactions + ": Credit on hold");
+		synchronized (toAccount) { //Deposit
+			toAccount.deposit(amount);
+			toAccount.notify();
+		}
+		System.out.println(nTransactions + ": Transfer complete");
+		nTransactions++;
+		if(nTransactions % NTEST == 0){
+			int sum = 0;
+			for(int i = 0; i < NACCOUNTS; i++){
+				sum += accounts[i].saldo();
+			}
+			System.out.println("Sum: " + sum);
+		}
 	}
-
+	public void run() throws InterruptedException{
+		for(int i = 0; i < NACCOUNTS; i++){
+			accounts[i].start();
+		}
+	}
 }
